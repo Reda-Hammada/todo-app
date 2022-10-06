295bd430766b3d7082bcf6d71868349d826e0eba
Reda Hammada <hammada.reda.dev@gmail.com>
2022-10-06 04:23:30 +0200

    uopdate

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,45 +4,53 @@
 
 
 @section('content')
-<div>
 
-    <form method="POST" action="{{ route('login') }}">
-     @csrf
+<main class="ultimate_container">
 
-
-        <label>Email</label>
-
-        <div>
-
-            <input type='text'
-                    name='email'>
-
+        <div class='form_container'>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+        
+        
+                <label>Email</label>
+        
+                <div>
+        
+                    <input type='text'
+                            name='email'>
+        
+                </div>
+        
+                <label>Password</label>
+        
+                <div>
+                    <input type="password"
+                            name="password" >
+                </div>
+                <div>
+                    <input type='checkbox' 
+                            name='remember'>
+                    <label>Remember me</label>
+        
+        
+                </div>
+                <div>
+                    <input type='submit'
+                            name='login'
+                            value='Log in'>
+                    <button><a href="{{ route('register') }}">Register</a></button>
+                </div>
+        
+            </form>
         </div>
-
-        <label>Password</label>
-
-        <div>
-            <input type="password"
-                   name="password" >
-        </div>
-        <div>
-            <input type='checkbox' 
-                   name='remember'>
-            <label>Remember me</label>
+        
+        <div class='bg_container'>
+            <h1>Welcome back</h1>
+       </div>
+            
+    </main>
 
 
-        </div>
-        <div>
-            <input type='submit'
-                   name='login'
-                   value='Log in'>
-            <button><a href="{{ route('register') }}">Register</a></button>
-        </div>
-
-    </form>
-
-
-</div>
 
 
 @endsection
