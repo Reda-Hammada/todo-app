832e809c5bc8d8dc33ff66e1b9fc70401c360612
Reda Hammada <hammada.reda.dev@gmail.com>
2022-10-06 02:42:12 +0200

    adding webpack mix to complie assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css" />
+
     <title>@yield('title')</title>
 </head>
 <body>
@@ -11,5 +13,7 @@
 
     @yield('content')
     
+    <script src="/js/app.js"></script>
+
 </body>
 </html>
