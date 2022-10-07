a778be3293b39af0dd664f3657fbc230e919ab74
Reda Hammada <hammada.reda.dev@gmail.com>
2022-10-07 18:14:18 +0200

    update adding image background to header

--- resources/views/task/dashboard.blade.php
+++ resources/views/task/dashboard.blade.php
@@ -6,29 +6,35 @@
 
 @section('content')
 
-<header>
-    <nav>
-        $user_id = Auth::id();
-
+<header class="header_background">
+    <nav class="navbar_container">
+        <div>
+            <h1>TODO</H1>
+        </div>
+            <img src="{{asset('images/icon-moon.svg')}}" alt="light mode icon" >
+        <div>
+        
+        </div>
     </nav>
+    <div>
+        <form method="POST" action = {{ route('task.store') }}>
+
+            @csrf
+
+            <div>
+                <input type='text' 
+                        name='task'>
+            </div>
+            <div>
+                <input type="submit"
+                       name="" 
+                       value="Create to do">
+        </form>
+    </div>
 </header>
 <main>
     <section>
-        <div>
-            <form method="POST" action = {{ route('task.store') }}>
-    
-                @csrf
-    
-                <div>
-                    <input type='text' 
-                            name='task'>
-                </div>
-                <div>
-                    <input type="submit"
-                           name="" 
-                           value="Create to do">
-            </form>
-        </div>
+     
          @foreach($tasks as $task)
         <div>
             <p>{{ $task['task_name'] }}</p>
