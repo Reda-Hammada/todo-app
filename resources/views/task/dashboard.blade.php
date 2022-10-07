2c1e2eac836967c9f1264501272baba9f5f11189
Reda Hammada <hammada.reda.dev@gmail.com>
2022-10-07 19:00:58 +0200

    styling the add task input

--- resources/views/task/dashboard.blade.php
+++ resources/views/task/dashboard.blade.php
@@ -8,27 +8,28 @@
 
 <header class="header_background">
     <nav class="navbar_container">
-        <div>
+        <div class="todo_heading_container">
             <h1>TODO</H1>
         </div>
+        <div class='mode_icon_container'>
             <img src="{{asset('images/icon-moon.svg')}}" alt="light mode icon" >
-        <div>
-        
         </div>
     </nav>
-    <div>
+    <div class="add_todo_form">
         <form method="POST" action = {{ route('task.store') }}>
 
             @csrf
 
             <div>
                 <input type='text' 
-                        name='task'>
+                        name='task'
+                        value="Create a new todo...">
             </div>
             <div>
                 <input type="submit"
-                       name="" 
+                       name="submit" 
                        value="Create to do">
+            </div>
         </form>
     </div>
 </header>
